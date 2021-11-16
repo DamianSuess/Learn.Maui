a667a7eb62b777527f97712c00203ef3541c22e3
Damian Suess <djsuess@gmail.com>
2021-11-16 15:22:11 -0500

    spacing

--- Learn.HelloMaui/Learn.HelloMaui/MainPage.xaml.cs
+++ Learn.HelloMaui/Learn.HelloMaui/MainPage.xaml.cs
@@ -4,21 +4,21 @@ using Microsoft.Maui.Essentials;
 
 namespace Learn.HelloMaui
 {
-	public partial class MainPage : ContentPage
-	{
-		int count = 0;
+  public partial class MainPage : ContentPage
+  {
+    int _count = 0;
 
-		public MainPage()
-		{
-			InitializeComponent();
-		}
+    public MainPage()
+    {
+      InitializeComponent();
+    }
 
-		private void OnCounterClicked(object sender, EventArgs e)
-		{
-			count++;
-			CounterLabel.Text = $"Current count: {count}";
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+      _count++;
+      CounterLabel.Text = $"Current count: {_count}";
 
-			SemanticScreenReader.Announce(CounterLabel.Text);
-		}
-	}
+      SemanticScreenReader.Announce(CounterLabel.Text);
+    }
+  }
 }
--- Learn.HelloMaui/Learn.HelloMaui/Platforms/Android/MainActivity.cs
+++ Learn.HelloMaui/Learn.HelloMaui/Platforms/Android/MainActivity.cs
@@ -4,8 +4,10 @@ using Microsoft.Maui;
 
 namespace Learn.HelloMaui
 {
-	[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
-	public class MainActivity : MauiAppCompatActivity
-	{
-	}
+  [Activity(Theme = "@style/Maui.SplashTheme",
+            MainLauncher = true,
+            ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+  public class MainActivity : MauiAppCompatActivity
+  {
+  }
 }
