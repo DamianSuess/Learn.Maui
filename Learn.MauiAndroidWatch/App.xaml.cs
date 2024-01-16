2954040bc7cbeee6225319a1194cbedbb9895ce7
Damian Suess <djsuess@gmail.com>
2024-01-16 11:55:54 -0500

    Base MAUI .NET 8 project template for Android Watch

---
+++ Learn.MauiAndroidWatch/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Learn.MauiAndroidWatch
+{
+  public partial class App : Application
+  {
+    public App()
+    {
+      InitializeComponent();
+
+      MainPage = new AppShell();
+    }
+  }
+}
